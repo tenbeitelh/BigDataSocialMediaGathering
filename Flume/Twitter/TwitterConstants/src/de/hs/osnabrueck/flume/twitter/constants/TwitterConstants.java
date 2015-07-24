0809e397356e39553fb873289f5156c9b7ed1e54
Holger Tenbeitel <tenbeitelholger@gmx.de>
2015-07-24 12:34:16 +0200

    TwitterConstants

---
+++ Flume/Twitter/TwitterConstants/src/de/hs/osnabrueck/flume/twitter/constants/TwitterConstants.java
@@ -0,0 +1,13 @@
+package de.hs.osnabrueck.flume.twitter.constants;
+
+
+public class TwitterConstants {
+	
+	public static final String CONSUMER_KEY = "consumer_key";
+	public static final String CONSUMER_SECRET = "consumer_secret";
+	public static final String ACCESS_TOKEN = "access_token";
+	public static final String ACCESS_SECRET = "access_secret";
+	public static final String TWITTER_KEYWORDS = "twitter_keywords";
+	public static final String TWITTER_USER_ACCOUNTS = "twitter_user_accounts";
+	
+}	
