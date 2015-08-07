d72d2648f8a3aeaec970ce67d1b6d374e1270cd2
Holger Tenbeitel <tenbeitelholger@gmx.de>
2015-08-07 09:50:15 +0200

    T

--- Flume/Facebook/src/de/hs/osnabrueck/htenbeitel/facebook/model/FacebookPost.java
+++ Flume/Facebook/src/de/hs/osnabrueck/htenbeitel/facebook/model/FacebookPost.java
@@ -1,5 +1,21 @@
 package de.hs.osnabrueck.htenbeitel.facebook.model;
 
+import java.util.Date;
+
+import facebook4j.Post;
+
 public class FacebookPost {
 
+	private Date createdTime;
+
+	public FacebookPost createFacebookPostObject(Post post) {
+		FacebookPost fPost = new FacebookPost();
+		fPost.setCreatedTime(post.getCreatedTime());
+		return fPost;
+	}
+
+	private void setCreatedTime(Date createdTime) {
+		// TODO Auto-generated method stub
+		this.createdTime = createdTime;
+	}
 }
--- Flume/Facebook/src/de/hs/osnabrueck/htenbeitel/flume/facebook/source/FacebookSource.java
+++ Flume/Facebook/src/de/hs/osnabrueck/htenbeitel/flume/facebook/source/FacebookSource.java
@@ -67,6 +67,8 @@ public class FacebookSource extends AbstractSource implements
 			}
 		};
 		
+		this.facebookReader.addListener(listener);
+		
 		
 	}
 
