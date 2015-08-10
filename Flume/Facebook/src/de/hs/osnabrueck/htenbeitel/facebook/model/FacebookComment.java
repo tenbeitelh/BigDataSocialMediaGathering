cbf2d8be7af155e8b7eb74a1827e146f12ee297a
Holger Tenbeitel <tenbeitelholger@gmx.de>
2015-08-10 11:47:46 +0200

    FacebookComment toJson()

--- Flume/Facebook/src/de/hs/osnabrueck/htenbeitel/facebook/model/FacebookComment.java
+++ Flume/Facebook/src/de/hs/osnabrueck/htenbeitel/facebook/model/FacebookComment.java
@@ -2,6 +2,8 @@ package de.hs.osnabrueck.htenbeitel.facebook.model;
 
 import java.util.Date;
 
+import com.google.gson.Gson;
+
 public class FacebookComment {
 	private String id;
 	private String from;
@@ -47,6 +49,68 @@ public class FacebookComment {
 
 	public void setLikeCount(Integer likeCount) {
 		this.likeCount = likeCount;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((createdTime == null) ? 0 : createdTime.hashCode());
+		result = prime * result + ((from == null) ? 0 : from.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((likeCount == null) ? 0 : likeCount.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FacebookComment other = (FacebookComment) obj;
+		if (createdTime == null) {
+			if (other.createdTime != null)
+				return false;
+		} else if (!createdTime.equals(other.createdTime))
+			return false;
+		if (from == null) {
+			if (other.from != null)
+				return false;
+		} else if (!from.equals(other.from))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (likeCount == null) {
+			if (other.likeCount != null)
+				return false;
+		} else if (!likeCount.equals(other.likeCount))
+			return false;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "FacebookComment [id=" + id + ", from=" + from
+				+ ", createdTime=" + createdTime + ", message=" + message
+				+ ", likeCount=" + likeCount + "]";
+	}
+
+	public String toJson() {
+		return new Gson().toJson(this);
 	}
 
 }
