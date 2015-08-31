ba8377a0afb1db99e83c0934f528426699fb0260
Holger Tenbeitel <tenbeitelholger@gmx.de>
2015-08-31 08:12:07 +0200

    HiveUtils

---
+++ Hive/HiveUtils/src/de/hs/osnabrueck/htenbeitel/hive/Main.java
@@ -0,0 +1,10 @@
+package de.hs.osnabrueck.htenbeitel.hive;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Hive/HiveUtils/src/de/hs/osnabrueck/htenbeitel/hive/utils/AddPartitions.java
@@ -0,0 +1,9 @@
+package de.hs.osnabrueck.htenbeitel.hive.utils;
+
+public class AddPartitions {
+	private String tableName;
+	
+	public AddPartitions(String tableName){
+		this.tableName = tableName;
+	}
+}
